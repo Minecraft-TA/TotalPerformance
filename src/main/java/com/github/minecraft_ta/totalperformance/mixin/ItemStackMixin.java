441bb5ba480eedf0be974cb9ea232448a63723dd
Pelotrio <45769595+Pelotrio@users.noreply.github.com>
2022-03-02 21:18:02 +0100

    Fixed Psi (should probably make a config but idc)

--- src/main/java/com/github/minecraft_ta/totalperformance/mixin/ItemStackMixin.java
+++ src/main/java/com/github/minecraft_ta/totalperformance/mixin/ItemStackMixin.java
@@ -9,13 +9,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import vazkii.psi.common.item.base.IPsiItem;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
 
     @Inject(method = "forgeInit", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/event/ForgeEventFactory;gatherCapabilities(Lnet/minecraft/item/ItemStack;Lnet/minecraftforge/common/capabilities/ICapabilityProvider;)Lnet/minecraftforge/common/capabilities/CapabilityDispatcher;", shift = At.Shift.BEFORE), remap = false, cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
     private void onForgeInit(CallbackInfo ci, Item item, ICapabilityProvider provider) {
-        if (provider == null && !(item instanceof IBauble)) {
+        if (provider == null && !(item instanceof IBauble) && !(item instanceof IPsiItem)) {
             ci.cancel();
         }
     }
